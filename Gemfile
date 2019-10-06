8c26e3a6dacc3f74b3944aafadb61bc97fcc3157
Ricardo Piro-Rael <ricardo@hint.com>
2019-10-06 12:18:39 -0700

    Use S3 for storage

--- Gemfile
+++ Gemfile
@@ -59,3 +59,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
+
+# Store files on S3
+gem 'aws-sdk-s3', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2019_10_06_110129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -45,14 +48,14 @@ ActiveRecord::Schema.define(version: 2019_10_06_110129) do
     t.text "description"
     t.text "alt_text"
     t.string "container_type", null: false
-    t.integer "container_id", null: false
+    t.bigint "container_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["container_type", "container_id"], name: "index_images_on_container_type_and_container_id"
   end
 
   create_table "timeline_posts", force: :cascade do |t|
-    t.integer "timeline_id", null: false
+    t.bigint "timeline_id", null: false
     t.text "title"
     t.text "description"
     t.text "body"
